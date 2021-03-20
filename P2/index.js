8dcb8825d67caaf3b6129a4ba302fd375c561559
kbenitez29 <kevin_beni1997@hotmail.com>
2021-03-20 08:09:45 +0100

    avance calculadora

--- P2/index.js
+++ P2/index.js
@@ -1,12 +1,54 @@
 console.log("Ejecutando JS...");
 
-//-- Obtener el párrafo
-const number = document.getElementById('fila 1');
-const display = document.getElementById('display');
+const numbers = document.getElementsByClassName("number");
+const display = document.getElementById("display");
+const operators = document.getElementsByClassName("operator");
+const sqrt = document.getElementById("sqrt");
+const equal = document.getElementById("equal");
+const dot = document.getElementById("dot");
+const del = document.getElementById("delete");
+const clear = document.getElementById("clear");
 
-//-- Funcion de retrollamada de la pulsación del párrafo
-number.onclick = () => {
-    console.log('number');
-    display.innerHTML+=number.value
+//-- Función de retrollamada de los botones
+//-- botones de la clase dígito
+function digit(value)
+{
+  console.log("Valor: " + value);
+}
 
+for (let number of numbers) {
+
+  //-- Establecer la funcion de llamada del boton i
+  //-- El parámetro ev.target contiene el boton
+  //-- que ha recibido el click
+  number.onclick = (ev) => {
+    digit(ev.target.value)
+    display.innerHTML+=(ev.target.value)
+  }
+}
+
+//-- Poner a cero la expresion
+clear.onclick = () => {
+  display.innerHTML = " ";
+}
+
+//-- Evaluar la expresion
+equal.onclick = () => {
+  display.innerHTML = eval(display.innerHTML);
+}
+
+for (let operator of operators) {
+  operator.onclick = (ev) => {
+    digit(ev.target.value)
+    display.innerHTML+=(ev.target.value)
+  }
+}
+
+//-- Raiz cuadrada definicion
+sqrt.onclick = () => {
+  display.innerHTML = Math.sqrt(display.innerHTML);
+}
+
+dot.onclick = () => {
+  display.innerHTML + ".";
 }
