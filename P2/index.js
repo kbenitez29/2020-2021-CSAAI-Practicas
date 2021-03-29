b4aabb8d778c07604efa65ae3bb26709cafb4196
kbenitez29 <kevin_beni1997@hotmail.com>
2021-03-29 08:35:39 +0200

    avance calculadora

--- P2/index.js
+++ P2/index.js
@@ -1,46 +1,91 @@
 console.log("Ejecutando JS...");
 
+//-- Elementos de la interfaz de la calculadora
 const numbers = document.getElementsByClassName("number");
 const display = document.getElementById("display");
 const operators = document.getElementsByClassName("operator");
 const sqrt = document.getElementById("sqrt");
 const equal = document.getElementById("equal");
-const dot = document.getElementById("dot");
+const dots = document.getElementById("dot");
 const del = document.getElementById("delete");
 const clear = document.getElementById("clear");
 
-//-- Función de retrollamada de los botones
-//-- botones de la clase dígito
-function digit(value)
-{
-  console.log("Valor: " + value);
+
+//-- Estados de la calculadora
+const ESTADO = {
+  INIT: 0,
+  OP1: 1,
+  OPERATION: 2,
+  OP2: 3,
 }
 
-for (let number of numbers) {
+//-- Variable de estado
+//-- Por defecto su valor será el del estado inicial
+let estado = ESTADO.INIT;
 
-  //-- Establecer la funcion de llamada del boton i
+
+//-- Función de retrollamada de los botones
+//-- botones de la clase dígito
+function digit(dig)
+{
+  if (estado == ESTADO.INIT) {
+    estado = ESTADO.OP1;
+    display.innerHTML = dig;
+    console.log("Valor: " + dig);
+    console.log("Estado: " + estado);
+  } else if(estado == ESTADO.OP1 || estado == ESTADO.OP2 ){
+    display.innerHTML += dig;
+    console.log("Valor: " + dig);
+    console.log("Estado: " + estado);
+  } else if (estado == ESTADO.OPERATION){
+    estado = ESTADO.OP2;
+    display.innerHTML += dig;
+    console.log("Valor: " + dig);
+    console.log("Estado: " + estado);
+  } 
+}
+  
+
+for (let number of numbers) {
   //-- El parámetro ev.target contiene el boton
   //-- que ha recibido el click
   number.onclick = (ev) => {
     digit(ev.target.value)
-    display.innerHTML+=(ev.target.value)
+    //display.innerHTML +=ev.target.value;
+  }
+}
+
+for (let operator of operators) {
+  operator.onclick = (ev) => {
+    if(estado == ESTADO.OP1){
+      digit(ev.target.value);
+      //display.innerHTML += (ev.target.value);
+      estado = ESTADO.OPERATION;
+      console.log("Estado: " + estado);
+    }
   }
 }
 
 //-- Poner a cero la expresion
 clear.onclick = () => {
   display.innerHTML = " ";
+  estado = ESTADO.INIT;
+  console.log("Valor: " + "clear");
+  console.log("Estado: " + estado);
+}
+
+//-- Borrar el último número de la expresión
+del.onclick = () =>{
+  display.innerHTML = display.innerHTML.slice(0,-1) // Slice coge elemento inicial y final a extraer en pantalla
 }
 
 //-- Evaluar la expresion
 equal.onclick = () => {
-  display.innerHTML = eval(display.innerHTML);
-}
-
-for (let operator of operators) {
-  operator.onclick = (ev) => {
-    digit(ev.target.value)
-    display.innerHTML+=(ev.target.value)
+  if(estado == ESTADO.OP2){
+    display.innerHTML = eval(display.innerHTML);
+    estado = ESTADO.OP1;
+    console.log("Valor: " + "equal");
+    console.log("Estado: " + estado);
   }
 }
 
@@ -49,6 +94,8 @@ sqrt.onclick = () => {
   display.innerHTML = Math.sqrt(display.innerHTML);
 }
 
-dot.onclick = () => {
-  display.innerHTML + ".";
+dots.onclick = () => {
+  display.innerHTML += ".";
 }
+
+//Mejorar el codigo
