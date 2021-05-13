ae5408082a34317e17394462dfe45a6350406ed9
kbenitez29 <kevin_beni1997@hotmail.com>
2021-05-13 11:10:11 +0200

    Avance P5, codigo js

--- P5/index.js
+++ P5/index.js
@@ -1,16 +1,38 @@
+// QUITAR 1 CAMARA , CREO QUE SOLO SE PONEN 2, NO 3
+
+
 //----- Obtener elemento de video y configurarlo
 let directo = document.getElementById("directo");
+
+// Videos
 const video1 = document.getElementById("video1");
+const video2 = document.getElementById("video2");
+const video3 = document.getElementById("video3");
+
+// Botones de camaras
 const btn_video1 = document.getElementById("btn_video1");
+const btn_video2 = document.getElementById("btn_video2");
+const btn_video3 = document.getElementById("btn_video3");
+
+// Boton test
 const btn_test = document.getElementById("btn_test");
-const btn_src_on = document.getElementById("btn1_src_on");
+
+// Botones encendido - apagado
+const btn_src_on = document.getElementById("btn_src_on");
+const btn_src_off = document.getElementById("btn_src_off");
 
 
 //-- Establecer las dimensiones de los vídeos
-directo.width=420;
-directo.height=200;
-video1.width=200;  
-video1.height=100;
+directo.width=720;
+directo.height=300;
+video1.width=300;  
+video1.height=150;
+
+video2.width=300;  
+video2.height=150;
+
+video3.width=300;  
+video3.height=150;
 
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "test.png";
@@ -19,23 +41,55 @@ const TEST_IMAGE_URL = "test.png";
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
+video3.poster = TEST_IMAGE_URL;
 
 //-- Boton de FUENTES-ON
 btn_src_on.onclick = () => {
  
-  //-- Establecer la fuente de la cámara 1
-  video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+    //-- Establecer la fuente de las camaras
+    video1.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4";
+    video2.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente2.mp4";
+    video3.src="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"; 
+    //-- Reprodeucimos un vídeo, desde el comienzo
+    video1.currentTime = 0;
+    video1.play();
 
-  //-- Reprodeucimos un vídeo, desde el comienzo
-  video1.currentTime = 0;
-  video1.play();
+    video2.currentTime = 0;
+    video2.play();
 
-  //-- Y en silencio...
-  video1.muted;
+    video3.currentTime = 0;
+    video3.play();
 
-  //-- En la emisión en directo ponemos la imagen de prueba
-  directo.poster = TEST_IMAGE_URL;
+    //-- Y en silencio...
+    video1.muted = true;
+    video2.muted = true;
+    video3.muted = true;
+
+    //-- En la emisión en directo ponemos la imagen de prueba
+    directo.poster = TEST_IMAGE_URL;
 };
+
+//-- Boton de FUENTES-OFF
+btn_src_off.onclick = () => {
+
+    // Muestra imagen test en directo
+    directo.poster = TEST_IMAGE_URL;
+    directo.src = null;
+
+    // Muestra imagen test en video 1
+    video1.poster = TEST_IMAGE_URL;
+    video1.src = null;
+
+    // Muestra imagen test en video 2
+    video2.poster = TEST_IMAGE_URL;
+    video2.src = null;
+
+    // Muestra imagen test en video 3
+    video3.poster = TEST_IMAGE_URL;
+    video3.src = null;
+}
+
 
 //-- Botón de Test
 btn_test.onclick = () => {
@@ -47,6 +101,22 @@ btn_test.onclick = () => {
 btn_video1.onclick = () => {
     directo.src = video1.src;
     directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+
+//-- Botón de Selección de la cámara 2
+btn_video2.onclick = () => {
+    directo.src = video2.src;
+    directo.currentTime = video1.currentTime;
+    directo.play();
+    directo.poster=null;
+};
+
+//-- Botón de Selección de la cámara 3
+btn_video3.onclick = () => {
+    directo.src = video3.src;
+    directo.currentTime = video3.currentTime;
     directo.play();
     directo.poster=null;
 };
